645da69da3561f2fa201813036b9f5dd4b17d489
Ali Joshany <aa6joshany@gmail.com>
2025-09-03 08:42:16 +0330

    add emit and props to edit modal

--- src/components/EditModal.vue
+++ src/components/EditModal.vue
@@ -1,23 +1,22 @@
 <template>
-    <div>
-        <div class="backdrop">
-        </div>
-        <div class="modal-main">
-           <input type="text" v-model="todoName">
-           <input type="text" v-model="progress">
-           <div>
-            <button>Edit</button>
-            <button>Close</button>
-           </div>
-        </div>
+  <div>
+    <div class="backdrop"></div>
+    <div class="modal-main">
+      <input type="text" v-model="todoName" />
+      <input type="text" v-model="progress" />
+      <div>
+        <button @click="emit('editComp', { todoName, progress })">Edit</button>
+        <button>Close</button>
+      </div>
     </div>
-    
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import './EditModal.css'
-const props = defineProps(['todoName','todoProgress'])
-let todoName = ref(props.todoName)
-let progress = ref(props.todoProgress)
+import { ref } from "vue";
+import "./EditModal.css";
+const props = defineProps(["todoName", "todoProgress"]);
+let todoName = ref(props.todoName);
+let progress = ref(props.todoProgress);
+const emit = defineEmits(["editComp"]);
 </script>
--- src/components/TodoComp.vue
+++ src/components/TodoComp.vue
@@ -5,30 +5,61 @@
       <input type="text" placeholder="Enter Your Task" v-model="newTodo" />
       <div>
         <input type="number" placeholder="%" min="0" v-model="progress" />
-        <button @click="addTodo">Add</button> 
+        <button @click="addTodo">Add</button>
       </div>
     </div>
 
     <div class="todo-categories">
-      <button @click="category = 'all'" class="categorybtn" :class="{activeCategory: category==='all'}">All</button>
-      <button @click="category = 'completed'" class="categorybtn" :class="{activeCategory: category==='completed'}" >Completed</button>
-      <button @click="category = 'uncompleted'" class="categorybtn" :class="{activeCategory: category==='uncompleted'}">Uncompleted</button>
+      <button
+        @click="category = 'all'"
+        class="categorybtn"
+        :class="{ activeCategory: category === 'all' }"
+      >
+        All
+      </button>
+      <button
+        @click="category = 'completed'"
+        class="categorybtn"
+        :class="{ activeCategory: category === 'completed' }"
+      >
+        Completed
+      </button>
+      <button
+        @click="category = 'uncompleted'"
+        class="categorybtn"
+        :class="{ activeCategory: category === 'uncompleted' }"
+      >
+        Uncompleted
+      </button>
     </div>
 
     <ul class="todo-list">
       <li v-for="(item, index) in filteredTodos" :key="index">
         <div class="todo-list-text">
-          <div class="todo-name" :class="{'complete':item.isCompleted}">
-            <input type="checkbox" @click="handleCompleted(item.id)" :checked="item.isCompleted"/>
+          <div class="todo-name" :class="{ complete: item.isCompleted }">
+            <input
+              type="checkbox"
+              @click="handleCompleted(item.id)"
+              :checked="item.isCompleted"
+            />
             {{ item.title }}
           </div>
           <div class="list-buttons">
-            <button @click="editTodo(index)" :style="{opacity: item.isCompleted ? 0.5 : 1}">⛏</button>
-            <button @click="removeTodo(index)" :style="{opacity: item.isCompleted ? 0.5 : 1}">❌</button>
+            <button
+              @click="editTodo(item)"
+              :style="{ opacity: item.isCompleted ? 0.5 : 1 }"
+            >
+              ⛏
+            </button>
+            <button
+              @click="removeTodo(index)"
+              :style="{ opacity: item.isCompleted ? 0.5 : 1 }"
+            >
+              ❌
+            </button>
           </div>
-
         </div>
-        <div class="progress" :style="{opacity: item.isCompleted ? 0.5 : 1}">
+        <div class="progress" :style="{ opacity: item.isCompleted ? 0.5 : 1 }">
           <div
             :class="['progress-bar', item.pBarClass]"
             :style="{ width: item.progress + '%' }"
@@ -36,87 +67,111 @@
         </div>
       </li>
     </ul>
-    <EditModal todoName="Agdas" todoProgress="50"  v-if="showEditModal"/>
-    
+    <EditModal
+      :todoName="editingTodo.title"
+      :todoProgress="editingTodo.progress"
+      v-if="showEditModal"
+      v-on:editComp="updateTodo"
+    />
   </div>
 </template>
 إ
 
 <script setup>
 import "./TodoComp.css";
-import EditModal from './EditModal.vue'
+import EditModal from "./EditModal.vue";
 import { computed, reactive, ref, toRef, watch } from "vue";
 
 let newTodo = ref("");
 let progress = ref();
 let todos = reactive([]);
-let showEditModal = ref(true)
-const category  = ref("all")
+let editingTodo = reactive({});
+let showEditModal = ref(false);
+const category = ref("all");
 
-const filteredTodos = computed(()=>{
-  if(category.value=="all") return todos
-  if(category.value==="completed") {
-    return todos.filter((item)=>{
-      return item.isCompleted===true
-    })
+const filteredTodos = computed(() => {
+  if (category.value == "all") return todos;
+  if (category.value === "completed") {
+    return todos.filter((item) => {
+      return item.isCompleted === true;
+    });
   }
-  if(category.value==="uncompleted"){
-    return todos.filter((item)=>{
-      return item.isCompleted===false
-    })
+  if (category.value === "uncompleted") {
+    return todos.filter((item) => {
+      return item.isCompleted === false;
+    });
   }
-})
+});
 
-function addTodo() {
+function changeProgressColor(progress) {
   let isRed = false;
   let isGreen = false;
   let isOrange = false;
+  if (progress.value <= 30) {
+    isRed = true;
+  }
+  if (progress.value > 30 && progress.value < 70) {
+    isRed = false;
+    isOrange = true;
+  }
+  if (progress.value >= 70) {
+    isOrange = false;
+    isGreen = true;
+    isRed = false;
+  }
+  if (progress.value >= 100) {
+    progress.value = 100;
+  }
+  if (isRed) return "red-progressBar";
+  if (isGreen) return "green-progressBar";
+  if (isOrange) return "orange-progressBar";
+  return "";
+}
+
+function addTodo() {
   if (newTodo.value.trim() !== "") {
-    if (progress.value <= 30) {
-      isRed = true;
-    }
-    if (progress.value > 30 && progress.value < 70) {
-      isRed = false;
-      isOrange = true;
-    }
-    if (progress.value >= 70) {
-      isOrange = false;
-      isGreen = true;
-      isRed = false;
-    }
-    if(progress.value>=100){
-      progress.value=100
-    }
     todos.push({
       id: Date.now(),
       title: newTodo.value,
       progress: progress.value,
       isCompleted: false,
-      pBarClass: computed(() => {
-        if (isRed) return "red-progressBar";
-        if (isGreen) return "green-progressBar";
-        if (isOrange) return "orange-progressBar";
-        return "";
-      }),
+      pBarClass: changeProgressColor(progress),
     });
     newTodo.value = "";
     progress.value = "";
-    category.value="all"
+    category.value = "all";
   }
-  console.log(todos)
-  console.log(filteredTodos)
+  // console.log(todos);
+  // console.log(filteredTodos);
 }
 
 function removeTodo(index) {
   todos.splice(index, 1);
 }
-function handleCompleted(id){
-  let todo = todos.find((item) =>{
-    return item.id===id
-  })
-  todo.isCompleted = !todo.isCompleted
+function handleCompleted(id) {
+  let todo = todos.find((item) => {
+    return item.id === id;
+  });
+  todo.isCompleted = !todo.isCompleted;
 }
 
+function editTodo(item) {
+  showEditModal.value = true;
+  editingTodo = { ...item };
+  // console.log(editingTodo);
+}
+
+function updateTodo(editedTodo) {
+  console.log(editedTodo);
+  let sTodo = todos.find((item) => item.id === editingTodo.id);
+  sTodo.title = editedTodo.todoName;
+  sTodo.progress = editedTodo.progress;
+  progress.value = editedTodo.progress;
+  sTodo.pBarClass = changeProgressColor(progress);
+  editingTodo.value = {};
+  showEditModal.value = false;
+  progress.value = "";
+}
 </script>
 
 <style scoped></style>
